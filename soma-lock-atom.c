b00daf31ea41740919cfae893619fc3c9a669c94
ladapata <109905748+ladapata@users.noreply.github.com>
2025-09-09 18:12:58 -0300

    Add files via upload

--- soma-lock-atom.c
+++ soma-lock-atom.c
@@ -2,22 +2,21 @@
 #include <stdlib.h> 
 #include <pthread.h>
 
+#define MAX_ITER 100000
+
 long int soma = 0;
 pthread_mutex_t mutex;
-
 pthread_cond_t cond; /* variavel de cond para bloqueio condicional*/
 
-int max_iter = 100000;
-short int is_finished = 1;
 
 void* ExecutaTarefa (void* args) {
   long int id = (long int) args;
   printf("Thread : %ld esta executando...\n", id);
 
-  for (int i=0; i<max_iter; i++) {
+  for (int i=0; i<MAX_ITER; i++) {
     pthread_mutex_lock(&mutex);
 
-    if(!(soma%1000) && is_finished) pthread_cond_wait(&cond, &mutex); /* condicao de bloqueio */
+    if(!(soma%1000)) pthread_cond_wait(&cond, &mutex); /* condicao de bloqueio */
 
     soma++;
     pthread_mutex_unlock(&mutex);
@@ -27,31 +26,27 @@ void* ExecutaTarefa (void* args) {
 }
 
 void* extra (void* args) {
-  int final = max_iter*(long int)args;
+  long int final = MAX_ITER*(long int)args;
   long int temp = 0;
+  int test_flag = 1;
   printf("Extra : esta executando...\n");
-  while (soma <= final) { /* condicao de loop foi mudada para ajudar EXTRA a nao terminar antes das outras threads*/
+  while(test_flag) { /* condicao de loop foi mudada para ajudar EXTRA a nao terminar antes das outras threads*/
     pthread_mutex_lock(&mutex);
     if (!(soma % 1000)){
       
-/* apenas para evitar de imprimir multiplos iguais */
-if(!(temp^soma)){pthread_cond_broadcast(&cond); pthread_mutex_unlock(&mutex); continue;}
-temp = soma;
-/* apenas para evitar de imprimir multiplos iguais */
+      /* apenas para evitar de imprimir multiplos iguais */
+      if(!(temp^soma)){pthread_cond_broadcast(&cond); pthread_mutex_unlock(&mutex); continue;}
+      temp = soma;
+      /* apenas para evitar de imprimir multiplos iguais */
 
       printf("logging : soma = %ld \n", temp);
 
       pthread_cond_broadcast(&cond); /* condicao foi atingida (soma foi impresso) */
 
+      test_flag = soma^final; /* condicao de loop foi mudada para ajudar EXTRA a nao terminar antes das outras threads*/
     }
     pthread_mutex_unlock(&mutex);
   }
-
-/* apenas para garantir que o programa sempre termine de executar caso thread EXTRA termina antes das outras */
-is_finished = 0;
-pthread_cond_broadcast(&cond);
-/* apenas para garantir que o programa sempre termine de executar caso thread EXTRA termina antes das outras */
-
   printf("Extra : terminou!\n");
   pthread_exit(NULL);
 }
